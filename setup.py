6e7e727026c0edf7159823550383192e3b8833dc
Jacqueline Garrahan <jacquelinegarrahan@gmail.com>
2022-01-24 17:32:07 -0800

    Add dev requirments

--- setup.py
+++ setup.py
@@ -8,6 +8,10 @@ with open(path.join(cur_dir, 'requirements.txt'), 'r') as f:
     requirements = f.read().split()
 
 
+# set up additional dev requirements
+with open(path.join(cur_dir, "dev-requirements.txt"), "r") as f:
+    dev_requirements = f.read().split()
+
 
 setup(
     name='lume-impact',
@@ -19,6 +23,9 @@ setup(
     long_description=open('README.md').read(),
     long_description_content_type='text/markdown',
     install_requires=requirements,
+    extras_require= {
+        "dev": dev_requirements,
+    },
     include_package_data=True,
     python_requires='>=3.6'
 )
